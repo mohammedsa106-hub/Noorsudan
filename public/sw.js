d91fdd2e2c9558e1edfd82d780a3b6116db5cac4
mohammedsa106-hub <mohammedsa106@gmail.com>
2026-08-03 15:36:19 +0300

    Updated sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nour-sudan-v2';
+const CACHE = 'nour-sudan-v3';
 const PRECACHE = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
